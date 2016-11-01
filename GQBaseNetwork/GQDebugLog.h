3d6357a8ea5497e4aa8e67a4bfb0f24ec6b77b8d
g763007297 <763007297@qq.com>
2016-11-01 16:44:35 +0800

    添加注释

--- GQBaseNetwork/GQDebugLog.h
+++ GQBaseNetwork/GQDebugLog.h
@@ -10,18 +10,47 @@
 
 @interface GQDebugLog : NSObject
 
+/**
+ 打印详细信息
+
+ @param message 提示信息
+ */
 + (void)printMessage:(NSString *)message;
 
+/**
+ 打印错误信息
+ 
+ @param message 提示信息
+ */
 + (void)errorMessage:(NSString *)message;
 
+/**
+ 打印警告信息
+ 
+ @param message 提示信息
+ */
 + (void)warningMessage:(NSString *)message;
 
+/**
+ 打印警告信息
+ 
+ @param message 提示信息
+ */
 + (void)infoMessage:(NSString *)message;
 
+/**
+ 初始化
+ */
 + (void)setUpConsoleLog;
 
+/**
+ 禁止打印log信息
+ */
 + (void)disableConsoleLog;
 
+/**
+ 打印log信息
+ */
 + (void)enableConsoleLog;
 
 @end
